1d6666100557d9e373cea0fe46457bb59025dd66
Zachary Levine <zlevine@zipari.com>
2019-05-16 12:44:05 -0400

    Total redo of list ops

--- test/listOperations.spec.ts
+++ test/listOperations.spec.ts
@@ -1,5 +1,24 @@
 import { expect } from 'chai';
+import * as listOps from './../src/listHelpers';
+
+interface User {
+  name: string;
+  age: number;
+  id: number;
+}
 
 describe('list operations', () => {
-  it('should do a thing', () => {});
+  let initialList: User[] = [];
+  beforeEach(() => {
+    initialList = [];
+  });
+
+  it('should add an item to the list', () => {
+    const itemToAdd: User = {
+      name: 'joe',
+      age: 37,
+      id: 3,
+    };
+    listOps.add<User>(initialList)(itemToAdd);
+  });
 });
